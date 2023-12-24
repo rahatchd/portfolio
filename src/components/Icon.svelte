b467f38f5adb40f5660ad85280c2b9a051c78417
Rahat Dhande <rahatchd@gmail.com>
2023-12-24 17:44:59 +0000

    format

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,45 +1,45 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
-  import { fade } from "svelte/transition";
+    import { createEventDispatcher } from "svelte";
+    import { fade } from "svelte/transition";
 
-  export let icon: any;
-  export let title: string = "untitled";
-  export let color: string = "grey";
+    export let icon: any;
+    export let title: string = "untitled";
+    export let color: string = "grey";
 
-  const dispatch = createEventDispatcher();
+    const dispatch = createEventDispatcher();
 </script>
 
 <div transition:fade on:dblclick={() => dispatch("launch", { title })}>
-  <span class="icon" style={`color: ${color};`}>
-    <svelte:component this={icon} />
-  </span>
-  <span class="caption">{title}</span>
+    <span class="icon" style={`color: ${color};`}>
+        <svelte:component this={icon} />
+    </span>
+    <span class="caption">{title}</span>
 </div>
 
 <style>
-  div {
-    width: 90px;
-    height: 90px;
-    padding: 10px;
-    margin: 5px 50px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border-radius: 5%;
-  }
-  div:hover {
-    background-color: rgba(0, 0, 0, 0.2);
-    cursor: pointer;
-  }
-  .icon {
-    display: inline-flex;
-    height: 40px;
-    width: 40px;
-  }
-  .caption {
-    margin-top: 5px;
-    font-size: 14px;
-    text-align: center;
-  }
+    div {
+        width: 90px;
+        height: 90px;
+        padding: 10px;
+        margin: 5px 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5%;
+    }
+    div:hover {
+        background-color: rgba(0, 0, 0, 0.2);
+        cursor: pointer;
+    }
+    .icon {
+        display: inline-flex;
+        height: 40px;
+        width: 40px;
+    }
+    .caption {
+        margin-top: 5px;
+        font-size: 14px;
+        text-align: center;
+    }
 </style>
--- src/components/Topbar.svelte
+++ src/components/Topbar.svelte
@@ -1,120 +1,140 @@
-<script lang=ts>
-  import { fly, fade } from 'svelte/transition';
-  import FaInstagram from 'svelte-icons/fa/FaInstagram.svelte';
-  import FaLinkedin from 'svelte-icons/fa/FaLinkedin.svelte';
-  import FaGithubSquare from 'svelte-icons/fa/FaGithubSquare.svelte';
+<script lang="ts">
+    import { fly, fade } from "svelte/transition";
+    import FaInstagram from "svelte-icons/fa/FaInstagram.svelte";
+    import FaLinkedin from "svelte-icons/fa/FaLinkedin.svelte";
+    import FaGithubSquare from "svelte-icons/fa/FaGithubSquare.svelte";
 
-  let dropdown: boolean = false;
-  const links: { href: string; text: string, icon: any }[] = [
-    { href: 'https://github.com/rahatchd', text: 'Github', icon: FaGithubSquare },
-    { href: 'https://www.linkedin.com/in/rahat-dhande-297122105/', text: 'Linkedin', icon: FaLinkedin },
-    { href: 'https://www.instagram.com/rahatchd/', text: 'Instagram', icon: FaInstagram },
-  ];
+    let dropdown: boolean = false;
+    const links: { href: string; text: string; icon: any }[] = [
+        {
+            href: "https://github.com/rahatchd",
+            text: "Github",
+            icon: FaGithubSquare,
+        },
+        {
+            href: "https://www.linkedin.com/in/rahat-dhande-297122105/",
+            text: "Linkedin",
+            icon: FaLinkedin,
+        },
+        {
+            href: "https://www.instagram.com/rahatchd/",
+            text: "Instagram",
+            icon: FaInstagram,
+        },
+    ];
 </script>
 
 <header>
-  <a href="#" on:click={() => dropdown = !dropdown}>
-  <img src={`./img/dropdown-${dropdown ? 'open' : 'closed'}.jpeg`} alt="avatar"/>
-  </a>
+    <a href="#" on:click={() => (dropdown = !dropdown)}>
+        <img
+            src={`./img/dropdown-${dropdown ? "open" : "closed"}.jpeg`}
+            alt="avatar"
+        />
+    </a>
 </header>
 {#if dropdown}
-  <div
-    class=dropdown
-    in:fly|fade={{ y: -10, duration: 500 }}
-    out:fly|fade={{ y: 10, duration: 500 }}
-  >
-    <p class=user>@rahatchd</p>
-    <ul>
-      {#each links as { href, text, icon }}
-      <li>
-        <a class=link {href}><div class=icon><svelte:component this={icon}/></div>&nbsp;{text}</a>
-      </li>
-      {/each}
-    </ul>
-    <p class=disclaimer>
-      ⚠️ Note: <i>site is under construction</i>
-    </p>
-  </div>
+    <div
+        class="dropdown"
+        in:fly|fade={{ y: -10, duration: 500 }}
+        out:fly|fade={{ y: 10, duration: 500 }}
+    >
+        <p class="user">@rahatchd</p>
+        <ul>
+            {#each links as { href, text, icon }}
+                <li>
+                    <a class="link" {href}
+                        ><div class="icon">
+                            <svelte:component this={icon} />
+                        </div>
+                        &nbsp;{text}</a
+                    >
+                </li>
+            {/each}
+        </ul>
+        <p class="disclaimer">
+            ⚠️ Note: <i>site is under construction</i>
+        </p>
+    </div>
 {/if}
 
 <style>
-  header {
-    top: 0;
-    width: 100vw;
-    height: 40px;
-    background-color: #222;
-    display: flex;
-    flex-direction: row-reverse;
-    align-items: center;
-  }
-  a {
-    padding: 3px 10px;
-    border-radius: 10%;
-    display: flex;
-    align-items: center;
-  }
-  a:hover {
-    background-color: rgba(200, 200, 200, 0.2);
-  }
-  img {
-    height: 25px;
-    width: 25px;
-    border-radius: 50%;
-    border: solid 1px white;
-  }
-  .dropdown {
-    position: absolute;
-    z-index: 100;
-    top: 50px;
-    right: 30px;
-    background-color: #eaeaea;
-    color: #222;
-    border-radius: 3px;
-    border: solid #222 1px;
-    width: 300px;
-    min-height: 5vh;
-    display: flex;
-    flex-direction: column;
-  }
-  .user {
-    margin: 10px 0 0 0;
-    padding: 0 10px 10px 10px;
-    border-bottom: solid #222 1px;
-  }
-  .disclaimer {
-    padding: 0 0 10px 10px;
-    margin-bottom: 5px;
-  }
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    border-bottom: solid #222 1px;
-  }
-  li {
-    margin: 1px 0;
-    padding: 10px 0;
-    border-bottom: rgba(0, 0, 0, 0.2) solid 1px;
-    display: flex;
-    flex-direction: row;
-  }
-  li:last-child {
-    border: none;
-  }
-  li:hover {
-    background-color: rgba(200, 200, 200, 0.5);
-  }
-  .link {
-    position: relative;
-    display: inline-flex;
-    text-decoration: none;
-    text-transform: none;
-    width: 100%;
-    color: #222;
-    background: none;
-  }
-  .icon {
-    width: 24px;
-    height: 24px;
-  }
+    header {
+        top: 0;
+        width: 100vw;
+        height: 40px;
+        background-color: #222;
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: center;
+    }
+    a {
+        padding: 3px 10px;
+        border-radius: 10%;
+        display: flex;
+        align-items: center;
+    }
+    a:hover {
+        background-color: rgba(200, 200, 200, 0.2);
+    }
+    img {
+        height: 25px;
+        width: 25px;
+        border-radius: 50%;
+        border: solid 1px white;
+    }
+    .dropdown {
+        position: absolute;
+        z-index: 100;
+        top: 50px;
+        right: 30px;
+        background-color: #eaeaea;
+        color: #222;
+        border-radius: 3px;
+        border: solid #222 1px;
+        width: 300px;
+        min-height: 5vh;
+        display: flex;
+        flex-direction: column;
+    }
+    .user {
+        margin: 10px 0 0 0;
+        padding: 0 10px 10px 10px;
+        border-bottom: solid #222 1px;
+    }
+    .disclaimer {
+        padding: 0 0 10px 10px;
+        margin-bottom: 5px;
+    }
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+        border-bottom: solid #222 1px;
+    }
+    li {
+        margin: 1px 0;
+        padding: 10px 0;
+        border-bottom: rgba(0, 0, 0, 0.2) solid 1px;
+        display: flex;
+        flex-direction: row;
+    }
+    li:last-child {
+        border: none;
+    }
+    li:hover {
+        background-color: rgba(200, 200, 200, 0.5);
+    }
+    .link {
+        position: relative;
+        display: inline-flex;
+        text-decoration: none;
+        text-transform: none;
+        width: 100%;
+        color: #222;
+        background: none;
+    }
+    .icon {
+        width: 24px;
+        height: 24px;
+    }
 </style>
